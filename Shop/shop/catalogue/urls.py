4733272b48c0fd291c817c39729dcaac0cfb621b
fana <fanabezabih@gmail.com>
2025-08-25 23:37:00 +0300

    Created a login and form to add products

--- Shop/shop/catalogue/urls.py
+++ Shop/shop/catalogue/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import list_products, product_detail, add_to_cart, cart_detail, remove_from_cart
+from django.contrib.auth.decorators import login_required
+from .views import list_products, product_detail, add_to_cart, cart_detail, remove_from_cart, create_product
+
+
+app_name = "catalogue"
 
 urlpatterns = [
+    path('products/create/', login_required(create_product), name="create_product"),
     path('products/', list_products, name='list_products'),
-    path('products/<int:id>/', product_detail, name='product_details'), 
+    path('products/<int:id>/', product_detail, name='product_details'),
     path('cart/add/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('cart/', cart_detail, name='cart_detail'),
     path('cart/remove/<int:product_id>/', remove_from_cart, name='remove_from_cart'),
--- Shop/shop/shop/settings.py
+++ Shop/shop/shop/settings.py
@@ -1,5 +1,3 @@
-
-
 """
 Django settings for shop project.
 
@@ -13,15 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from pathlib import Path
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -32,13 +24,16 @@ SECRET_KEY = 'django-insecure-y&ae0i(#w2h1xqwcgxmqv4crgblt7fp%#$l#2&1%v$ajplmpv+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Application definition
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'catalogue:list_products'
+LOGOUT_REDIRECT_URL = 'login'
+STATIC_URL = '/static/'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -67,7 +62,8 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # This tells Django to look for templates in your project-level templates folder
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
@@ -92,25 +87,15 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
@@ -122,7 +107,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
